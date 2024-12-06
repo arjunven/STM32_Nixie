bafc90310a97473ef186ac80083247520641891e
Arjun Venkatesh <arjun.venn@gmail.com>
2024-12-05 21:34:32 -0800

    Mucking with cmake files to get clangd working right

--- firmware/cmake/gcc-arm-none-eabi.cmake
+++ firmware/cmake/gcc-arm-none-eabi.cmake
@@ -1,15 +1,19 @@
 set(CMAKE_SYSTEM_NAME               Generic)
 set(CMAKE_SYSTEM_PROCESSOR          arm)
 
+# Enforce compiler ID checks
 set(CMAKE_C_COMPILER_FORCED TRUE)
 set(CMAKE_CXX_COMPILER_FORCED TRUE)
 set(CMAKE_C_COMPILER_ID GNU)
 set(CMAKE_CXX_COMPILER_ID GNU)
 
-# Some default GCC settings
-# arm-none-eabi- must be part of path environment
-set(TOOLCHAIN_PREFIX                arm-none-eabi-)
+# Set C++ standard
+set(CMAKE_CXX_STANDARD 11)
+set(CMAKE_CXX_STANDARD_REQUIRED ON)
+set(CMAKE_CXX_EXTENSIONS OFF)
 
+# Set toolchain paths
+set(TOOLCHAIN_PREFIX                /opt/ST/STM32CubeCLT_1.16.0/GNU-tools-for-STM32/bin/arm-none-eabi-)
 set(CMAKE_C_COMPILER                ${TOOLCHAIN_PREFIX}gcc)
 set(CMAKE_ASM_COMPILER              ${CMAKE_C_COMPILER})
 set(CMAKE_CXX_COMPILER              ${TOOLCHAIN_PREFIX}g++)
@@ -17,6 +21,7 @@ set(CMAKE_LINKER                    ${TOOLCHAIN_PREFIX}g++)
 set(CMAKE_OBJCOPY                   ${TOOLCHAIN_PREFIX}objcopy)
 set(CMAKE_SIZE                      ${TOOLCHAIN_PREFIX}size)
 
+# Set executable suffix
 set(CMAKE_EXECUTABLE_SUFFIX_ASM     ".elf")
 set(CMAKE_EXECUTABLE_SUFFIX_C       ".elf")
 set(CMAKE_EXECUTABLE_SUFFIX_CXX     ".elf")
@@ -24,25 +29,47 @@ set(CMAKE_EXECUTABLE_SUFFIX_CXX     ".elf")
 set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
 
 # MCU specific flags
-set(TARGET_FLAGS "-mcpu=cortex-m3 ")
+set(TARGET_FLAGS "-mcpu=cortex-m3")
 
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${TARGET_FLAGS}")
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wall -Wextra -Wpedantic -fdata-sections -ffunction-sections")
+# Common flags for all languages
+set(COMMON_FLAGS 
+    ${TARGET_FLAGS}
+    -Wall
+    -Wextra 
+    -Wpedantic
+    -fdata-sections
+    -ffunction-sections
+)
+
+# Debug/Release specific flags
 if(CMAKE_BUILD_TYPE MATCHES Debug)
-    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -O0 -g3")
+    list(APPEND COMMON_FLAGS "-O0" "-g3")
+else()
+    list(APPEND COMMON_FLAGS "-Os" "-g0")
 endif()
-if(CMAKE_BUILD_TYPE MATCHES Release)
-    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Os -g0")
-endif()
 
-set(CMAKE_ASM_FLAGS "${CMAKE_C_FLAGS} -x assembler-with-cpp -MMD -MP")
-set(CMAKE_CXX_FLAGS "${CMAKE_C_FLAGS} -fno-rtti -fno-exceptions -fno-threadsafe-statics")
+# Convert list to space-separated string
+string(REPLACE ";" " " COMMON_FLAGS_STR "${COMMON_FLAGS}")
 
-set(CMAKE_C_LINK_FLAGS "${TARGET_FLAGS}")
-set(CMAKE_C_LINK_FLAGS "${CMAKE_C_LINK_FLAGS} -T \"${CMAKE_SOURCE_DIR}/STM32F100C4Tx_FLASH.ld\"")
-set(CMAKE_C_LINK_FLAGS "${CMAKE_C_LINK_FLAGS} --specs=nano.specs")
-set(CMAKE_C_LINK_FLAGS "${CMAKE_C_LINK_FLAGS} -Wl,-Map=${CMAKE_PROJECT_NAME}.map -Wl,--gc-sections")
-set(CMAKE_C_LINK_FLAGS "${CMAKE_C_LINK_FLAGS} -Wl,--start-group -lc -lm -Wl,--end-group")
-set(CMAKE_C_LINK_FLAGS "${CMAKE_C_LINK_FLAGS} -Wl,--print-memory-usage")
+# Set language specific flags
+set(CMAKE_C_FLAGS "${COMMON_FLAGS_STR}")
+set(CMAKE_ASM_FLAGS "${COMMON_FLAGS_STR} -x assembler-with-cpp")
+set(CMAKE_CXX_FLAGS "${COMMON_FLAGS_STR} -fno-rtti -fno-exceptions -fno-threadsafe-statics")
 
-set(CMAKE_CXX_LINK_FLAGS "${CMAKE_C_LINK_FLAGS} -Wl,--start-group -lstdc++ -lsupc++ -Wl,--end-group")
+# Use nano specs for linking
+set(CMAKE_EXE_LINKER_FLAGS_INIT "")
+
+# Core linker flags
+list(APPEND CMAKE_EXE_LINKER_FLAGS_INIT
+    "${TARGET_FLAGS}"
+    "-T\"${CMAKE_SOURCE_DIR}/STM32F100C4Tx_FLASH.ld\""
+    "-Wl,-Map=${CMAKE_PROJECT_NAME}.map,--gc-sections"
+    "-Wl,--print-memory-usage"
+)
+
+# Convert list to space-separated string
+string(REPLACE ";" " " CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS_INIT}")
+
+# Set nano spec through proper CMake variable
+set(CMAKE_C_STANDARD_LIBRARIES "--specs=nano.specs")
+set(CMAKE_CXX_STANDARD_LIBRARIES "--specs=nano.specs -Wl,--start-group -lstdc++ -lsupc++ -Wl,--end-group")
