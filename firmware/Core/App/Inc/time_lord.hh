575b282c081ec09e508520eec4502b7fb33f2fa1
Arjun Venkatesh <arjun.venn@gmail.com>
2024-12-22 20:58:36 -0800

    Start clock from 0

--- firmware/Core/App/Inc/time_lord.hh
+++ firmware/Core/App/Inc/time_lord.hh
@@ -10,9 +10,7 @@
 
 class Time_lord {
  public:
-  Time_lord(Nixie_display& display,
-            RTC_HandleTypeDef* hrtc,
-            RTC_TimeTypeDef initial_time);
+  Time_lord(Nixie_display& display, RTC_HandleTypeDef* hrtc);
 
   /** @brief Gets time from RTC and updates the display to the current time  */
   bool update();
@@ -26,7 +24,7 @@ class Time_lord {
  private:
   Nixie_display& display_;
   RTC_HandleTypeDef* hrtc_;
-  RTC_TimeTypeDef time_;
+  RTC_TimeTypeDef time_;  // Starts from 00:00:00
 };
 
 #endif
